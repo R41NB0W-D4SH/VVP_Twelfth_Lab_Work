567e3a95ad206513f51daf7beb6e3c5b46618773
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-12 13:08:06 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+void Power3A(double a, double &b)
+{
+	b = pow(a, 3);
+}
+
+int main()
+{
+	setlocale(0, "");
+	double b, a;
+	cout << "Введите число A" << endl;
+	cin >> a;
+
+	Power3A(a, b);
+	cout << "Итог равен " << b << endl;
+
+	system("pause");
+	return 0;
+}
