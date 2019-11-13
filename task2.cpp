56ad1d752c1639dd6e7331161a177ed79a0d6424
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-14 00:37:29 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-void Sign(x)
+int Sign(double x)
 {
-
+	if (x < 0)
+	{
+	return -1;
+	}
+	if (x > 0)
+	{ 
+	return 1;
+	}
+	return 0;
 }
 
 int main()
 {
+	setlocale(0, "");
+	double a, b;
+	
+	cout << "Введите числа A и B" << endl;
+	cin >> a >> b;
+
+	cout << "Итог равен " << Sign(a) + Sign(b) << endl;
+
 	system("pause");
 	return 0;
 }
