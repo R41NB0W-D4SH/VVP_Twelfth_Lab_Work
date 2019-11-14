960cb4835aba21279daa4d222fd135030caeaadb
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-14 22:00:44 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+double Fact2(int n)
+{
+	int i = 2 - n % 2;
+	double sum = 1.0;
+	for (; i <= n; i = i + 2)
+	{
+		sum = sum * i;
+	}
+	return sum;
+}
+
+int main()
+{
+	setlocale(0, "");
+	int n;
+	cout << "Введите число N" << endl;
+	cin >> n;
+
+	cout << "Факториал равен " << Fact2(n) << endl;
+
+	system("pause");
+	return 0;
+}
